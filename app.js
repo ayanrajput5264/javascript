5e38d62e3093bec7f5008938855d494ef4ed6e02
ayanrajput5264 <ayanait87@gmail.com>
2026-04-12 00:37:31 +0500

    1st js assainment done

---
+++ app.js
@@ -0,0 +1,19 @@
+//  question no 1
+
+ alert("Error! Please Enter valid Password")
+
+//  question no 2
+
+alert("Welcome to js land.. \n Happy Coading")
+
+// question no 3
+
+alert("Welcome to js land")
+
+// question no 4
+
+alert("Prevent this page from creating additional dailouges")
+
+// question no 5
+
+alert("Hello... i can run js through my web browser's console")
