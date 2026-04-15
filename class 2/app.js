98e2d2aaa5c916804ba1228833d6eefd8dee7ac0
ayanrajput5264 <ayanait87@gmail.com>
2026-04-15 19:53:07 +0500

    all asnwer done

--- class 2/app.js
+++ class 2/app.js
@@ -45,4 +45,28 @@
 
 // question no 5
 
-let 
+// let pzzo = "pizza \n pizz \n piz \n pi"
+// alert(pzzo)
+
+// question no 6
+
+// let email
+// email = "My email rao717767@gmail.com"
+// alert(email)
+
+// question no 7
+
+// let book
+// book = "A smarter way to learn javasscript"
+// alert(book)
+
+// qyestion no 8 
+
+// let text = "Yah i can write HTML content trough javascript"
+// document.write("ya ah i can write HTML content trough javascript")
+
+// question no 9 
+
+// let design
+// design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
