95d6e176ece45ef06b8e7bac1a28863493fef910
ayanrajput5264 <ayanait87@gmail.com>
2026-04-20 18:39:43 +0500

    3rd assaigment done

--- class 2/app.js
+++ class 2/app.js
@@ -2,71 +2,73 @@
 
 // question npo 1
 
-// let userName 
+let userName 
 
 // question no 2
+
 // string value
 
-// let myName = "Ayan"
+let myName = "Ayan"
 
 // question no 3
+
 // sec A
 
-// let message
+let message
 
 // sec B
 
-// message = "hello world"
+message = "hello world"
 
 // sec C 
 
-// alert(message)
+alert(message)
 
 // question no 4
 
 // sec A
 
-// let age
-// age = "18"
-// alert(age)
+let age
+age = "18"
+alert(age)
 
 // sec B
 
-// let myName 
-// myName = "rao ayan"
-// alert(myName)
+let myName 
+myName = "rao ayan"
+alert(myName)
 
 // sec C 
 
-// let course
-// course = "web development"
-// alert(course)
+let course
+course = "web development"
+alert(course)
 
 
 // question no 5
 
-// let pzzo = "pizza \n pizz \n piz \n pi"
-// alert(pzzo)
+let pzzo = "pizza \n pizz \n piz \n pi"
+alert(pzzo)
 
 // question no 6
 
-// let email
-// email = "My email rao717767@gmail.com"
-// alert(email)
+let email
+email = "My email rao717767@gmail.com"
+alert(email)
 
 // question no 7
 
-// let book
-// book = "A smarter way to learn javasscript"
-// alert(book)
+let book
+book = "A smarter way to learn javasscript"
+alert(book)
 
 // qyestion no 8 
 
-// let text = "Yah i can write HTML content trough javascript"
-// document.write("ya ah i can write HTML content trough javascript")
+let text = "Yah i can write HTML content trough javascript"
+document.write("ya ah i can write HTML content trough javascript")
 
 // question no 9 
 
-// let design
-// design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
-// alert(design)
+let design
+design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
---
+++ class 4/app.js
@@ -0,0 +1,42 @@
+// opreters
+
+// let num1 = "10"
+// let num2 = 3
+
+// let num3 = "2"
+
+
+// console.log(num1-num2+num3)
+
+// // concetination 
+
+
+// let age = 18
+
+// alert("i am "+age + "years old")
+
+// question no 2
+
+// let num = 23
+// alert("you have visited this site "+num  )
+
+// question no 3
+
+let birthYear = 2007
+let type = typeof birthYear
+document.write
+// question no 4 
+
+let visitorName = "ahmed bhai"
+let productTitle = "shirt"
+let quality = 10
+
+document.write(`${visitorName} orderd ${quality} ${productTitle} on urbanzilla clothin store`)
+
+
+
+
+
+
+
+
