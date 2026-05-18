fe504aebbb9e9f68ebdb8bfb271c43d199458d0c
ayanrajput5264 <ayanait87@gmail.com>
2026-05-18 18:47:53 +0500

    done

---
+++ class 9/app.js
@@ -0,0 +1,24 @@
+// array
+
+// let studentNames = ["ayan","rao","rajput",756,true,643,false ,true,7575,75,75]
+
+// // // index number 0 1 2 3 4 5 6 7
+
+// // console.log(studentNames)
+
+// // studentNames[4] = false
+// // console.log(studentNames[studentNames.length - 1])
+// // // pop
+// // // push
+
+
+// studentNames.pop()
+
+// studentNames.push("anus","asad")
+
+// studentNames.shift()
+
+// studentNames.unshift("zyan")
+// console.log(studentNames)
+
+
---
+++ classs 7/app.js
@@ -0,0 +1,61 @@
+// let userAge = prompt("apke age kya he")
+
+// if ( userAge >= 18) {
+//     alert("jao maze karo=")
+// }
+// else{
+//     alert("bare hp r a")
+// }
+
+// let password = "1234admin"
+
+// let userPassword = prompt("Enter Your Password")
+// if (userPassword == password){
+//     alert("welcome")
+// }
+// else{
+//     alert("incerrect password")
+// }
+
+
+// let city = prompt("where are you living") 
+// let education = prompt("what your education")
+
+// if(city == "karachi" && education=="matric"){
+//     alert("you are hired")
+// }
+// else{
+//     alert("come to karachi")
+// }
+
+
+// nesting 
+
+
+// let num = 3
+
+// let userNumber = prompt("guess the number")
+// if (userNumber == num){
+//     alert("good")
+// }
+// else{
+//     alert("try again")
+// }
+
+
+let khuahish = prompt("kya khana he")
+
+if (khuahish === "biryani"){
+    alert("2 plate le ao")
+}
+else if (khuahish === "samosa"){
+    let quantity = prompt("kitne le ao")
+    alert(`${quantity }`)
+}
+else if (khuahish === "fries"){
+    alert("50 ki le a")
+}
+else{
+    alert("lys le a")
+}
+
